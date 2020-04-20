fa5d7cbdc76ff96cfbccb7af636c1e9982d9ff62
Andy Cao <caozsn@gmail.com>
2020-04-20 17:40:52 +1000

    Storage: get environment values (for Docker)

--- ZDeals.Storage.Picture/ServiceConfigure/StorageConfigure.cs
+++ ZDeals.Storage.Picture/ServiceConfigure/StorageConfigure.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using System;
+using System.IO;
 using ZDeals.Storage.FileSystem;
 
 namespace ZDeals.Storage.Picture.ServiceConfigure
@@ -9,10 +10,14 @@ namespace ZDeals.Storage.Picture.ServiceConfigure
     {
         public static void AddStorageServices(this IServiceCollection services, IConfiguration configuration)
         {
-
             var storageOptions = new FileSystemStorageOptions();
             configuration.GetSection("FileSystemStorageOptions").Bind(storageOptions);
 
+            var directory = Environment.GetEnvironmentVariable("STORAGE_DIRECTORY");
+            if (!string.IsNullOrWhiteSpace(directory))
+            {
+                storageOptions.Directory = directory;
+            }
             services.AddScoped<IBlobService>(options =>
             {
                 return new FileSystemBlobService(storageOptions);
